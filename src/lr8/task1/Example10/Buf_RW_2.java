0a0907e3bbc13eec5cbda1f0c0b29ef6499076e6
artem <usernameartemf@gmail.com>
2024-02-17 16:16:20 +0500

    initial commit

---
+++ src/lr8/task1/Example10/Buf_RW_2.java
@@ -0,0 +1,37 @@
+package lr8.task1.Example10;
+
+import java.io.*;
+
+public class Buf_RW_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = null;
+        PrintWriter out = null;
+        try {
+            br = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream("C:\\MyFile1.txt"), "cp1251"));
+
+            out = new PrintWriter("C:\\MyFile2.txt", "cp1251");
+
+            int lineCount = 0;
+            String s;
+
+            while ((s = br.readLine()) != null) {
+                lineCount++;
+                out.println(lineCount + ":" + s);
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Ошибка!!!");
+        }
+        finally {
+            br.close();
+            out.flush();
+            out.close();
+        }
+    }
+}
+
+//Пример 10. Выполнить чтение из одного файла и запись в другой
+//файл с использованием класса PrintWriter.
---
+++ src/lr8/task1/Example8/InConvertInText.java
@@ -0,0 +1,57 @@
+package lr8.task1.Example8;
+
+import java.io.*;
+import java.net.URL;
+
+public class InConvertInText {
+
+    public static void readAllByByte(Reader in) throws IOException {
+        while (true) {
+            int oneByte = in.read();
+            if (oneByte != -1) {
+                System.out.print((char) oneByte);
+            } else {
+                System.out.print("\n" + "конец");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            InputStream inFile = new FileInputStream("C:\\MyFile1.txt");
+
+            Reader rFile = new InputStreamReader(inFile, "cp1251");
+
+            readAllByByte(rFile);
+            System.out.print("\n\n\n");
+            inFile.close();
+            rFile.close();
+
+            InputStream inUrl = new URL("http://google.com").openStream();
+
+            Reader rUrl = new InputStreamReader(inUrl, "cp1251");
+
+            readAllByByte(rUrl);
+            System.out.print("\n\n\n");
+            inUrl.close();
+            rUrl.close();
+
+            InputStream inArray = new ByteArrayInputStream(new byte[] {5, 8, 3, 9, 11});
+            Reader rArray = new InputStreamReader(inArray, "cp1251");
+
+            readAllByByte(rArray);
+            System.out.print("\n\n\n");
+            inArray.close();
+            rArray.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка: " + e);
+        }
+    }
+}
+
+// Пример 8. Прочитать и вывести на экран информацию из трех источников: файла
+//        на диске, интернет-страницы и массива данных типа byte. Указать кодировку,
+//поддерживающую кириллицу. (Сравнить с работой программы, приведенной в
+//примере 2.)
+
